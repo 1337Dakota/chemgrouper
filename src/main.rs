802fbe88ea409fd92d60a25fc7b123938e0a9b3a
Dakota <72140289+1337Dakota@users.noreply.github.com>
2024-12-15 15:52:00 +0100

    Now running a loop and you can pass an argument

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
+use std::env::args;
+
 use chemgrouper::{build_graph, build_steps, parse_json};
 
 fn main() {
     let json = include_str!("../out.json");
     let chemicals = parse_json(json);
-    let choices = chemicals.iter().map(|c| c.name()).collect();
+    let choices: Vec<String> = chemicals.iter().map(|c| c.name()).collect();
+    let arg_target = args().nth(1);
+    if let Some(target) = arg_target {
+        if let Some(target) = chemicals
+            .iter()
+            .find(|c| c.name().to_lowercase() == target.to_lowercase())
+        {
+            let graph = build_graph(target.clone());
+            let steps = build_steps(graph);
+            println!("{steps}");
+            return;
+        } else {
+            eprintln!("Target not found");
+            return;
+        }
+    }
 
-    let target = inquire::Select::new("Select Target", choices)
-        .prompt()
-        .unwrap();
-    let target = chemicals
-        .iter()
-        .find(|c| c.name() == target)
-        .unwrap()
-        .clone();
+    loop {
+        let target = inquire::Select::new("Select Target", choices.clone())
+            .prompt_skippable()
+            .unwrap();
+        let target = match target {
+            Some(v) => chemicals.iter().find(|c| c.name() == v).unwrap().clone(),
+            None => break,
+        };
 
-    let graph = build_graph(target);
-    let steps = build_steps(graph);
-    println!("{steps}");
+        let graph = build_graph(target);
+        let steps = build_steps(graph);
+        println!("{steps}");
+    }
 }
